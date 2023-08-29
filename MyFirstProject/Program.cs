fa54ba82201c1700a49f79c1e443e2b02d0a3514
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-28 20:36:47 -0600

    Finish

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -30,28 +30,63 @@
             // Console.WriteLine(gravity);
 
             // 👉 Operators
-            Console.WriteLine(1 + 1);
-            Console.WriteLine(10 >= 5);
-            Console.WriteLine(1 - 3);
-            Console.WriteLine(2 * 2);
-            Console.WriteLine(10 / 2);
-            Console.WriteLine(5 == 5);
-            Console.WriteLine(2 != 5);
-            Console.WriteLine(1 < 5);
+            //Console.WriteLine(1 + 1);
+            //Console.WriteLine(10 >= 5);
+            //Console.WriteLine(1 - 3);
+            //Console.WriteLine(2 * 2);
+            //Console.WriteLine(10 / 2);
+            //Console.WriteLine(5 == 5);
+            //Console.WriteLine(2 != 5);
+            //Console.WriteLine(1 < 5);
 
-            Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
 
-            Console.WriteLine(true && false);
-            Console.WriteLine(false && true);
-            Console.WriteLine(true && true);
-            Console.WriteLine(false && false);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(false && true);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
 
-            Console.WriteLine("--------------------------");
+            //Console.WriteLine("--------------------------");
 
-            Console.WriteLine(true || true);
-            Console.WriteLine(true || false);
-            Console.WriteLine(false || true);
-            Console.WriteLine(false || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || true);
+            //Console.WriteLine(false || false);
+
+
+            // 👉 User Input
+            //Console.WriteLine("Enter your name: ");
+            //string nombre = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + nombre);
+
+
+            // 👉 Conditionals
+            //age = 18;
+
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("You are an adult");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are not an adult");
+            //}
+
+            // Switch
+            age = 17;
+
+            switch (age)
+            {
+                case 18:
+                    Console.WriteLine("You are an adult.");
+                    break;
+                case 16:
+                    Console.WriteLine("You are a teenager.");
+                    break;
+                default:
+                    Console.WriteLine("You are a minor.");
+                    break;
+            }
         }
     }
 }
